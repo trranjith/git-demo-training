bfbee55a82c5c72cda96cb08b0f2ca9f389c6f01
tanveezahamad <tanveez.ahamad@accenture.com>
2026-01-30 12:32:09 +0530

    modified by me

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 class Example {
 	public static void main(String [] args) {
-		System.out.println("Hello Welcome to the training...");
+		System.out.println("Hello Welcome to the training...........................");
 		System.out.println("Sun rises in the East.");
 	}
 }
